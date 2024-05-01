582ca4bb45c9d965de29c84efb9968470f6d7d17
sara-dme <s.daoumy@gamil.com>
2024-05-01 06:12:05 +0100

    tsk 0

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
+
+describe('calculateNumber', () => {
+  it('Return 4.', function() {
+    assert.strictEqual( calculateNumber(1, 3), 4);
+  });
+});
+describe('Calculate float and integer:', function() {
+  it('return 5.', function() {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+});
+describe('Calculate -2 and -1.7:', function() {
+    it('return -4.', function() {
+      assert.strictEqual(calculateNumber(-2, -1.7), -4);
+    });
+});
+describe('Calculate float and float:', function() {
+    it('return 6.', function() {
+      assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
+});
