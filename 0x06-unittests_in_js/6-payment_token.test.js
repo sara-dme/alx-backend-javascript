bd80a674ce2ed3203a9764992e878b968a2255e3
sara-dme <s.daoumy@gamil.com>
2024-05-01 19:10:12 +0100

    fix task 6

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -5,7 +5,7 @@ describe('getPaymentTokenFromAPI', function () {
   it('return a promise', function (done) {
     getPaymentTokenFromAPI(true)
     .then((res) => {
-      assert.equal(res.data, 'Successful response from API');
+      assert.equal(res.data, 'Successful response from the API');
       done();
     })
     .catch((error) => done(error));
