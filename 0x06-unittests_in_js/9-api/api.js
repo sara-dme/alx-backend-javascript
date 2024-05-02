9b1f415730274dfedda1ed67e08049daccf2e1aa
sara-dme <s.daoumy@gamil.com>
2024-05-02 14:34:38 +0100

    fix task 9

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id([0-9]+', (req, res) => {
+app.get('/cart/:id([0-9]+)', (req, res) => {
     res.send(`Payment methods for cart ${req.params.id}`);
 });
 
