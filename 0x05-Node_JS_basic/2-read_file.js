b3baba6601b3a78d413b969eb3e215e1a85d693c
sara-dme <s.daoumy@gamil.com>
2024-05-02 19:02:07 +0100

    fix

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,30 +1,26 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  try{
+  try {
     const data = fs.readFileSync(path, 'utf-8');
     const lines = data.trim().split('\n');
     const hashtable = {};
 
     for (const line of lines) {
       const fields = line.split(',');
-      const field  = fields[fields.length - 1].trim();
+      const field = fields[fields.length - 1].trim();
 
-      if (field === '') {
-        continue;
-      }
       if (!hashtable[field]) {
-        hashtable[field] = []
+        hashtable[field] = [];
       }
       hashtable[field].push(fields[0]);
-
     }
     console.log('Number of students:', lines.length - 1);
     for (const [field, students] of Object.entries(hashtable)) {
       console.log(`Number of students in ${field}:${students.length}. List: ${students.join(', ')}`);
     }
-    } catch (err) {
-      throw new Error('Cannot load the database');
-    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
 module.exports = countStudents;
