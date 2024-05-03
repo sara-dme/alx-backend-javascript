1cce0538ef2c2b31a11e745caf148d4f08def7b2
sara-dme <s.daoumy@gamil.com>
2024-05-03 10:37:42 +0100

    fix task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,51 +1,53 @@
 const express = require('express');
-const fs = require('fs').promises;
+const fs = require('fs');
 
 const app = express();
 
 function countStudents(path) {
-  return fs.readFile(path, 'utf-8')
-    .then(data => {
-      const lines = data.trim().split('\n').filter(line => line.trim() !== '');
-      const students = lines.map(line => line.split(','));
-      const hashtable = {};
-      const totalStudents = students.length - 1;
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      }
+      else {
+        let output = '';
+        const lines = data.trim().split('\n').filter(line => line.trim() !== '');
+        const students = lines.map(line => line.split(','));
+        const hashtable = {};
+        const totalStudents = students.length - 1;
 
-      students.forEach(student => { 
-        const field = student[3];
-        if (!hashtable[field]) {
-          hashtable[field] = [];
-        }
-        hashtable[field].push(student[0]);
+        students.forEach(student => { 
+          const field = student[3];
+          if (!hashtable[field]) {
+            hashtable[field] = [];
+          }
+          hashtable[field].push(student[0]);
         });
-        const output = [];
-
-        output.push(`Number of students: ${totalStudents}`);
+        output += `Number of students: ${totalStudents}\n`;
         for (const field in hashtable) {
           if (field != 'field') {
             const num = hashtable[field].length;
-            output.push(`Number of students in ${field}: ${num}. List: ${hashtable[field].join(', ')}`);
+            output += `Number of students in ${field}: ${num}. List: ${hashtable[field].join(', ')}\n`;
           }
         }
-        return output.join('\n');
-      })
-      .catch(error => {
-        throw new Error('Cannot load the database');
-      });
-  }
+        resolve(output);
+      }
+    });
+  });
+}
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 app.get('/students', (req, res) => {
-  countStudents(process.argv[2])
+  countStudents(process.argv[2].toString())
     .then((data) => {
-      res.send(`This is the list of our students\n${data}`)
+      res.send(['This is the list of our students', data].join('\n'));
     })
-    .catch((error) => {
-      res.status(500).send(`This is the list of our students\n${error.message}`);
-    });
-});
+    .catch(() => {
+      res.send('This is the list of our students\nCannot load the database');
+    })
+})
 app.listen(1245);
 
 module.exports = countStudents;
